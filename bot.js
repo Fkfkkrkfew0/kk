6b47e4452d75f7b9b51bfd064f019f0ff036069d
Fkfkkrkfew0 <44093001+Fkfkkrkfew0@users.noreply.github.com>
2018-10-19 02:51:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,17 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("490591334575636482")
+setInterval(function() {
+
+ for i in range (0,10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000):
  
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
-
-
-
-
+channel.send(`شهد `);
+}, 25)
+})
  
-bot = commands.Bot(command_prefix='!')
- 
-
-
-    print("Bot Is Starting Up... Please Wait")
-    print("Name: " + bot.user.name)
-    print("ID: " + bot.user.id)
-    print("Bot Is Online! And Ready To Spam")
- 
-bot.command(pass_context=True)
-
-    await bot.say("Hi There!")
-    time.sleep(2)
-    for i in range (0,10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000):
-        await bot.say("hi noob")
  
 client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
